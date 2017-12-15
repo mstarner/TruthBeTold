f7aa69f89362e9895f6dda1b9b4298fe65641b47
Matthew Starner <mstarner8@gmail.com>
2017-12-14 22:20:36 -0500

    Add files via upload

---
+++ TruthBeTold/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        System.out.print("Write in your boolean statement");
+        String input = kb.nextLine();
+
+    }
+}
